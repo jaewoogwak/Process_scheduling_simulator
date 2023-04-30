038455a4550fb38fc52c19187c1b7ab8804f68c3
jaewoogwak <jaewoo010207@gmail.com>
2023-05-01 01:53:19 +0900

    RR 구현: 알고리즘 구현 완료

--- Algorithm/RR.py
+++ Algorithm/RR.py
@@ -1,59 +1,196 @@
-# AT(arriveTime), BT(BurstTime), RT(RemainingTime), WT(waitingTime), TT(TurnaroundTime), NTT(Normalized Turnaround Time)
-processList = {'p1':[0, 3, 0, 0, 0], 'p2':[1, 7, 0, 0, 0], 'p3':[3, 2, 0, 0, 0], 'p4':[5, 5, 0, 0, 0], 'p5':[6, 3, 0, 0, 0]}
-quantum = 2
+def isFinished(completed):
+    for i in range(len(completed)):
+        if not completed[i]:
+            return False
+        
+    return True
 
-# AT(arriveTime), BT(BurstTime), RT(RemainingTime), WT(WaitingTime), TT(Turnaroundtime), NTT(Normalized Turnaround Time)
-for key, value in processList.items():
-    processList[key] = {'AT': value[0], 'BT': value[1], 'WT': value[2], 'TT': value[3], 'NTT': value[4]}
 
-queue = []
-time = 0
+def setPreemption(i, processor, allocated):
+    # if processor[i][3] == timeQuantum:
+    print("프로세서",i+1,"에서 선점 발생")
+    processor[i][0] = False
+    processor[i][2] = -1
+    processor[i][3] = 0
 
-# 각 프로세스의 AT, BT, RT, WT, TT, NTT 값을 계산합니다.
-for key in processList:
-    process = processList[key]
-    process['RT'] = process['BT']
-    process['WT'] = 0
-    process['TT'] = 0
-    process['NTT'] = 0
 
-# 프로세스를 큐에 추가하고, 실행시간을 계산합니다.
-while True:
-    for key in processList:
-        process = processList[key]
-        if process['AT'] <= time and process['RT'] > 0 and key not in queue:
-            queue.append(key)
+# 기본 로직
+# 1. 종료할 프로세스 있는지 확인 
+# 2. ready Q 넣기
+# 3. ready Q에서 빼기
+# 4. 프로세스에게 프로세서 할당 
+# 5. 실행
+# 6. Time quantum 지나면 현재 프로세서의 실행 멈추고 2번으로 넘어감
 
-    if not queue:
-        break
+def RoundRobin(inputInfo, arrival_time, workLoad, timeQuantum):
+    N, core = inputInfo
+    P = len(core) # 프로세서
+    readyQueue = []
+    
+    # 프로세스 할당 받은 여부, 코어의 종류, 현재 실행중인 프로세스 번호, timeSlice
+    # on = True, off = False
+    processor = []
+    for i in range(P):
+        processor.append([False, core[i], -1, 0])    
+    
+    prevState = [False for _ in range(P)]
+    notArrived = [True for _ in range(N)]
+    completed = [False for _ in range(N)]
+    allocated = [False for _ in range(N)]
+    burstTime = [0 for _ in range(N)]
+    isOccurPreemption = [False for _ in range(N)]
+    
+    burstTimeTemp = workLoad[:]
+    waitingTime = [0] * N
+    turnaroundTime = [0] * N
+    normalizedTT = [0] * N 
+    
+    
+    currentTime = 0
+    consumedPower = 0
+    temp = []
+    
+    while not isFinished(completed):
+        # readyQueue = []
+        p = 0
+        
+        print("---",currentTime,"초---", workLoad)
+        
+        # 종료할 프로세스가 있는지 확인
+        for i in range(P):
+            p = processor[i][2]
+        
+            if workLoad[p] <= 0:
+                if processor[i][2] != -1:
+                    completed[p] = True
+                    print("*** 프로세스", processor[i][2]+1, " 종료 ***")
+                    turnaroundTime[p] = currentTime - arrival_time[p]
+                    processor[i][0] = False
+                    processor[i][2] = -1
+                    processor[i][3] = 0
 
-    currentProcess = queue.pop(0)
-    process = processList[currentProcess]
-    if process['RT'] <= quantum:
-        time += process['RT']
-        process['WT'] = time - process['AT'] - process['BT']
-        process['TT'] = time - process['AT']
-        process['NTT'] = process['TT'] / process['BT']
-        process['RT'] = 0
-    else:
-        time += quantum
-        process['RT'] -= quantum
-        process['WT'] = time - process['AT'] - process['BT']
-        process['TT'] = time - process['AT']
-        process['NTT'] = process['TT'] / process['BT']
-        queue.append(currentProcess)
+        if isFinished(completed):
+            print("종료!")
+            break
+        
+        # 선점당할 프로세스 있는지 확인
+        preemption = False
+        toBePreemption = []
+        for i in range(P):
+            if processor[i][3] == timeQuantum:
+                p = processor[i][2]
+                toBePreemption.append(p)
+                processor[i][2] = -1
+                isOccurPreemption[p] = True
+                allocated[p] = False
+                preemption = True
+                processor[i][0] = False
+        
+        # ready queue에 넣기
+        for i in range(N):
+            if arrival_time[i] <= currentTime and not completed[i] and not allocated[i] and notArrived[i]:
+                readyQueue.append(i)
+                notArrived[i] = False
+                
+        if preemption:
+            print("선점 발생")
+            for p in toBePreemption:
+                readyQueue.append(p)
+                isOccurPreemption[p] = False
+            preemption = False
+            
+        print("RQ", readyQueue)
 
-    for key in processList:
-        process = processList[key]
-        if process['AT'] > time or process['RT'] == 0:
-            continue
-        if key not in queue and key != currentProcess:
-            process['WT'] += quantum
+        # ready queue에서 빼기            
+        for i in range(P):
+            if not processor[i][0]:
+                if len(readyQueue) > 0: 
+                    p = readyQueue.pop(0)
+                
+                # 아직 프로세서 할당 못받은 프로세스라면, 할당 받음
+                if not allocated[p] and processor[i][0] == False and p != -1:
+                    processor[i][2] = p
+                    processor[i][0] = True
+                    print("프로세스",p+1,"는 프로세서를",i+1,"할당받음")           
+                    allocated[p] = True
+                    processor[i][3] = 0
+                    
+        # 할당받고
+        print("할당받은뒤 프로세서", processor)
+        
+        for i in range(P):
+            if prevState[i] == False and processor[i][0] ==  True:
+                print("### 프로세서",i+1, "ON ###")
+                prevState[i] = True
+                
+                if processor[i][1] == 'E':
+                    consumedPower += 0.1
+                    
+                else:
+                    consumedPower += 0.5
+        
+        # 프로세서 할당 받은 프로세스는 실행함
+        for i in range(P):
+            p = processor[i][2]   
+            if processor[i][0]:
+                
+                if processor[i][1] == 'E':
+                    workLoad[p] -= 1
+                    consumedPower += 1
+                    burstTime[p] += 1
+                
+                else:
+                    workLoad[p] -= 2
+                    consumedPower += 3
+                    burstTime[p] += 1
+                
+                # timeSlice += 1
+                processor[i][3] += 1
+                    
+                print("프로세서",i+1 ,": 프로세서",p+1, "처리")
+            
+            if workLoad[p] <= 0:
+                workLoad[p] = 0    
 
-print("Process\tAT\tBT\tWT\tTT\tNTT")
-for key in processList:
-    process = processList[key]
-    print("{}\t{}\t{}\t{}\t{}\t{}".format(key, process['AT'], process['BT'], process['WT'], process['TT'], round(process['NTT'], 2)))
-totalTT = sum([processList[key]['TT'] for key in processList])
-averageTT = totalTT / len(processList)
-print("Average response time:", round(averageTT, 2))
+        currentTime += 1
+        
+        for i in range(P):
+            if prevState[i] == True and processor[i][0] == False and len(readyQueue) == 0:
+                print("### 프로세서",i+1, "OFF ###")
+                prevState[i] = False
+        
+        # Ready Q에서 대기 중인 애들 waitingTime += 1
+        for i in readyQueue:
+            waitingTime[i] += 1
+        
+        print("실행하지 못한애들 Ready Q", readyQueue)
+        print("소비전력", consumedPower)
+
+        print() 
+        
+        if currentTime > 25:
+            return
+
+    # Nomalized TT 구하기
+    for i in range(N):
+        normalizedTT[i] = turnaroundTime[i] / burstTime[i]
+        
+    print("소비전력", consumedPower)
+    print("실행시간", burstTime)
+    print("대기시간", waitingTime)
+    print("반환시간", turnaroundTime)
+    print("Nomalized TT", normalizedTT)
+    
+
+if __name__ == "__main__":
+    process = 5
+    processor = ['P', 'P']
+    inputInfo = (process, processor)
+    
+    arrivalTime = [0, 1, 3, 5, 6]
+    workLoad = [3,7,2,5,3]
+    
+    timeQuantum = 3
+    
+    
+    RoundRobin(inputInfo, arrivalTime, workLoad, timeQuantum)
