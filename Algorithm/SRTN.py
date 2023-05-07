be7b5b1a25b8956c9478bdb7ed9f81ecde97987e
BaeJia <124412137+BaeJia@users.noreply.github.com>
2023-05-07 22:16:16 +0900

    리팩터링: main에서 스케줄링 알고리즘 실행할 수 있도록 변경

--- Algorithm/SRTN.py
+++ Algorithm/SRTN.py
@@ -1,22 +1,11 @@
-
-# 기본 로직
-# 1. 종료할 프로세스 있는지 확인 
-# 2. ready Q 넣기
-# 3. ready Q에서 빼기
-# 3-1. 하나 있으면 바로 꺼내기
-# 3-2. 여러 개 있으면 workload 작은 프로세스 꺼내기
-# 4. 프로세스에게 프로세서 할당 
-# 4-1. 전부 실행중이면 선점
-# 5. 실행
+def isFinished(completed):
+    for i in range(len(completed)):
+        if not completed[i]:
+            return False
+        
+    return True
 
 def SRTN(inputInfo, arrivalTime, workLoad):
-    
-    def isFinished(completed):
-        for i in range(len(completed)):
-            if not completed[i]:
-                return False
-            
-        return True
 
     N, core = inputInfo # 프로세스 수, 프로세서 정보
     P = len(core) # 프로세서 수
@@ -63,31 +52,31 @@ def SRTN(inputInfo, arrivalTime, workLoad):
             print("종료!")
             break
         
-        # ready queue에 넣기
+        # Ready queue에 넣기
         for i in range(N):
             if arrivalTime[i] <= currentTime and not completed[i] and not allocated[i] and notArrived[i]:
                 readyQueue.append(i)
                 notArrived[i] = False
                 
                 
-        # ready queue에서 빼기 
+        # Ready queue에서 빼기 
                      
         for i in range(P): 
             
             processor_full = 1 # 프로세서 전부 가동중인지 확인 
             for k in range(P):
                 if processor[k][0] == False :
-                    processor_full=0
+                    processor_full = 0
              
             if processor_full == 1:
                 break       
                     
-            if len(readyQueue) == 1:  # ready queue에 하나 있으면 바로 꺼냄
+            if len(readyQueue) == 1:  # Ready queue에 하나 있으면 바로 꺼냄
                 p = readyQueue.pop(0)  
-            elif len(readyQueue) >= 2: # ready queue에 여러개 있으면
-                min=workLoad[readyQueue[0]]
-                index=0
-                for j in range(1,len(readyQueue)):
+            elif len(readyQueue) >= 2: # Ready queue에 여러개 있으면
+                min = workLoad[readyQueue[0]]
+                index = 0
+                for j in range(1, len(readyQueue)):
                     if min > workLoad[readyQueue[j]]:
                         index = j
                 p=readyQueue.pop(index) # workload 가장 적은 프로세스 꺼냄
@@ -96,8 +85,8 @@ def SRTN(inputInfo, arrivalTime, workLoad):
             if not allocated[p] and processor[i][0] == False and p != -1:                     
                 processor[i][2] = p
                 processor[i][0] = True
-                processor[i][3]= workLoad[p] 
-                print("프로세스",p+1,"는 프로세서를",i+1,"할당받음")           
+                processor[i][3] = workLoad[p] 
+                print("프로세스", p+1, "는 프로세서를", i+1, "할당받음")           
                 allocated[p] = True
             elif processor[i][0] == 1:
                 readyQueue.append(p)
@@ -105,43 +94,43 @@ def SRTN(inputInfo, arrivalTime, workLoad):
             
         if processor_full == 1 : # 프로세서가 다 가동중이면
             
-            if len(readyQueue) == 1:  # ready queue에 하나 있으면 바로 꺼냄
+            if len(readyQueue) == 1:  # Ready queue에 하나 있으면 바로 꺼냄
                 p = readyQueue.pop(0)  
-            elif len(readyQueue) >= 2: # ready queue에 여러개 있으면
-                min=workLoad[readyQueue[0]]
-                index=0
-                for j in range(1,len(readyQueue)):
+            elif len(readyQueue) >= 2: # Ready queue에 여러개 있으면
+                min = workLoad[readyQueue[0]]
+                index = 0
+                for j in range(1, len(readyQueue)):
                     if min > workLoad[readyQueue[j]]:
                         index = j
-                p=readyQueue.pop(index) # workload 가장 적은 프로세스 꺼냄
+                p = readyQueue.pop(index) # workload 가장 적은 프로세스 꺼냄
             
             max_workload = processor[0][3]
-            running_process=processor[0][2]
-            processor_num=0
+            running_process = processor[0][2]
+            processor_num = 0
             
             for l in range(1, P):
                 if processor[l][3] > max_workload:
-                    max_workload=processor[l][3]
-                    running_process=processor[l][2]
-                    processor_num=l
+                    max_workload = processor[l][3]
+                    running_process = processor[l][2]
+                    processor_num = l
                     
-            if max_workload > workLoad[p]: # 레디 큐에서 나온 프로세스 workload와 비교
+            if max_workload > workLoad[p]: # Ready queue에서 나온 프로세스 workload와 비교
                 readyQueue.append(running_process)   
                 allocated[running_process] = False                
                 processor[processor_num][2] = p
                 processor[processor_num][0] = True
-                processor[processor_num][3]= workLoad[p] 
-                print("프로세스",p+1,"는 프로세서를",i+1,"할당받음")           
+                processor[processor_num][3] = workLoad[p] 
+                print("프로세스", p+1, "는 프로세서를", i+1, "할당받음")           
                 allocated[p] = True
                 
-            else : # 레디 큐에서 꺼내온 프로세스 다시 레디 큐로 삽입
+            else : # Ready queue에서 꺼내온 프로세스 다시 Ready queue로 삽입
                 readyQueue.append(p)
                             
                     
-        
+        # 시동할 프로세스 있는지 확인
         for i in range(P):
-            if prevState[i] == False and processor[i][0] ==  True:
-                print("### 프로세서",i+1, "ON ###")
+            if prevState[i] == False and processor[i][0] == True:
+                print("### 프로세서", i+1, "ON ###")
                 prevState[i] = True
                 
                 if processor[i][1] == 'E':
@@ -165,31 +154,30 @@ def SRTN(inputInfo, arrivalTime, workLoad):
                     consumedPower += 3
                     burstTime[p] += 1
                     
-                print("프로세서",i+1 ,": 프로세서",p+1, "처리")
+                print("프로세서", i+1 , ": 프로세서", p+1, "처리")
             
             if workLoad[p] <= 0:
                 workLoad[p] = 0  
             
             processor[i][3]= workLoad[p]
 
+        # 현재 시간 증가
         currentTime += 1
         
+        # 시동 종료할 프로세서 있는지 확인
         for i in range(P):
             if prevState[i] == True and processor[i][0] == False and len(readyQueue) == 0:
-                print("### 프로세서",i+1, "OFF ###")
+                print("### 프로세서", i+1, "OFF ###")
                 prevState[i] = False
         
         # Ready Q에서 대기 중인 애들 waitingTime += 1
         for i in readyQueue:
             waitingTime[i] += 1
-        
-        # print("실행하지 못한애들 Ready Q", readyQueue)
-        # print("소비전력", consumedPower)
-            
 
+        
         print() 
 
-    # 다 끝내고 Nomalized TT 구하기
+    # Nomalized TT 구하기
     for i in range(N):
         normalizedTT[i] = turnaroundTime[i] / burstTime[i]
         
@@ -197,51 +185,3 @@ def SRTN(inputInfo, arrivalTime, workLoad):
     output = [burstTime, waitingTime, turnaroundTime, normalizedTT, consumedPower]
     return output
     
-def generateProcessor(core, P, E):
-    if core != P + E:
-        print("프로세서 분배가 올바르지 않습니다.")
-        return False
-    
-    processor = []
-    for i in range(P):
-        processor.append("P")
-
-    for i in range(E):
-        processor.append("E")
-        
-    return processor
-
-def checkValidate(process, arrivalTime, workLoad):
-    if process != len(arrivalTime) or process != len(workLoad):
-        return False
-
-    return True
-
-
-if __name__ == "__main__":
-    process = 5
-    core = 1
-    Pcore = 0
-    Ecore = 1
-    processor = generateProcessor(core, Pcore, Ecore)
-    inputInfo = (process, processor)
-    arrivalTime = [0,1,3,5,6]
-    workLoad = [3,7,2,5,3]
-    # arrivalTime = [0, 0, 1, 3, 3, 4, 4, 6, 8, 9]
-    # workLoad = [10, 5, 7, 5, 8, 12, 13, 6, 3, 9]
-
-    # 입력이 올바른지 검사
-    if not checkValidate(process, arrivalTime, workLoad):
-        print("프로세스 수와 arrivalTime, workLoad 데이터 길이가 일치하지 않습니다.")
-
-    else:
-        # 프로세스 스케줄링 실행
-        output = SRTN(inputInfo, arrivalTime, workLoad)
-        burstTime, waitingTime, turnaroundTime, normalizedTT, consumedPower = output
-
-        print("소비전력", consumedPower)
-        print("실행시간", burstTime)
-        print("대기시간", waitingTime)
-        print("반환시간", turnaroundTime)
-        print("Nomalized TT", normalizedTT)
-        print("output", output)
